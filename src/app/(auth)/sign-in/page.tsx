906c4b3d92307512c27d73fa38ab6f785d61190b
tanvir-rana09 <tanvir.rana.soikat@gmail.com>
2024-04-19 21:31:43 +0600

    minor bug

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,19 +2,20 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+  // const { data: session } = useSession()
+  // if (session) {
+  //   return (
+  //     <>
+  //       Signed in as {session.user.email} <br />
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </>
+  //   )
+  // }
+  // return (
+  //   <>
+  //     Not signed in <br />
+  //     <button onClick={() => signIn()}>Sign in</button>
+  //   </>
+  // )
+  return <h1>hi</h1>
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,10 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    if (!token &&  url.pathname.startsWith("/dashboard") ) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
 }
 
 // See "Matching Paths" below to learn more
